10f8fdc9ac3f08574cf372d703757cd2d82fa8aa
Klaudia <klaudia.bodyk@icloud.com>
2024-01-04 21:06:05 +0100

    #2 - Add FolderList, Inputs, update package.json

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Bar from "./Bar";
+import Inputs from "./Inputs";
+import FolderList from "./FolderList";
+import {Fab} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 const Dashboard = () => {
     return(
         <>
             <Bar />
+            <Inputs />
+            <Fab color="primary" aria-label="add" size={"small"}>
+                <AddIcon />
+            </Fab>
+            <FolderList />
         </>
     )
 }
